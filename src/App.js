86400bc626d992a1fde6d0791932287088fa828a
Smile Gupta <smile@codeops.tech>
2020-10-16 22:44:23 +0530

    Header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './components/ui/Header'
 import './App.css';
+
 
 const App = () => {
   return (
     <div className="container">
-     Hello World
+     <Header />
     </div>
   );
 }
